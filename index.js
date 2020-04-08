e012df98b65c66b3cd2ed0b0c20d4ba19b0e4263
Shamir Gibbons <shamir.gibbons@gmail.com>
2020-04-08 14:17:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function yourAnimal(animal) {
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
   var animal= 'cat'
-  return cat
+  return animal
 }
 
 function add2(n) {
